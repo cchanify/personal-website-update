5726a9fdc32bf8ead70fac496ded9163775b4ba4
Connor <chanify@middlebury.edu>
2018-10-07 23:03:14 -0400

    cleaned up projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Container, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Media } from 'reactstrap';
 import styled from 'styled-components';
 
 import img from './images/crater.jpg';
@@ -8,10 +9,11 @@ const Home = (props) => {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">My name is Connor and I'm a senior computer science at Middlebury College</p>
+        {/* <h1 className="display-3">Hello, world!</h1> */}
+        <p className="lead">My name is Connor and I'm a senior computer science major at Middlebury College</p>
         <hr className="my-2" />
-        <p>During my four years here I helped captain the Men's Ultimate Frisbee Team, participated in Middlebury's computer science club, MiddleEndian, and helped tutor students taking Data Structures and Computer Architecutre.</p>
+        <p>During my four years at Midd I helped captain the Men's Ultimate Frisbee Team, participated in Middlebury's computer science club, MiddleEndian, and tutored students taking Data Structures and Computer Architecutre.</p>
+        <p>I'm passionate about building sustainable, highly functional products that help people live better lives. My computer science coursework has been directed along a systems track, and I have experience writing low level C programs and peeling back the layers of abstraction that make computers and user applications work. My interests lie in system security, blockchain technology. I'm a fan of the <a href="https://www.dreamsongs.com/RiseOfWorseIsBetter.html" target="_"> Worse Is Better</a> UNIX philosophy and a lover of clean, minimal interfaces. </p>
         <p className="lead">
           <Button color="primary">Learn More</Button>
         </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { Card, Button, CardTitle, CardImg, CardText, Row, Col } from 'reactstrap';
+import { Card, Button, CardTitle, CardImg, CardText, Row, Col, Container } from 'reactstrap';
 
 
 class Projects extends Component {
   render() {
     return (
-      <Row>
+      <Container>
+        <Col sm="12" md={{ size: 8, offset: 2 }} >
+          <Row>
+            <Card body outline color="secondary">
+              <CardTitle>Endeavor</CardTitle>
+              <CardText>A web application to track job application progress. Created with Chris Fetterolf as a semester long project for our capstone senior work in the computer science major.</CardText>
+              <Button color="primary" target="_blank" href="https://endeavorapp.herokuapp.com/" >Endeavor</Button>
+            </Card>
+          </Row>
+
+          <Row>
+            <Card body outline color="secondary">
+              <CardTitle>NASA Picture of the Day</CardTitle>
+              <CardText>A small web application that utilizes a publicly available NASA API to display a different picture and description of our universe each day. Adapted for React from an online guide.</CardText>
+              <Button color="primary" >Go somewhere</Button>
+            </Card>
+          </Row>
+
+          <Row>
+            <Card body outline color="secondary">
+              <CardTitle>Todo List</CardTitle>
+              <CardText>A todo list I made to learn the basics of React and Javascript.</CardText>
+              <Button color="primary" target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/">
+              Todo App
+              </Button>
+            </Card>
+          </Row>
+
+          <Row>
+            <Card body outline color="secondary">
+              <CardTitle>Tic Tac Toe</CardTitle>
+              <CardText>A small application made from the React documentation. </CardText>
+              <Button color="primary" target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/" >Tic Tac Toe</Button>
+            </Card>
+          </Row>
+        </Col>
+      </Container>
+
+
+
+      /* <Row>
         <Col sm="6">
           <Card body>
             <CardTitle>Endeavor</CardTitle>
@@ -36,7 +76,7 @@ class Projects extends Component {
             <Button>Go somewhere</Button>
           </Card>
         </Col>
-      </Row>
+      </Row> */
 
     )
   }
