e1d1766003a57735ba882ba312fdb43434d57e40
Connor <chanify@middlebury.edu>
2018-10-08 22:50:31 -0400

    reformatted projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,14 +16,14 @@ const Home = (props) => {
         <h1 className="display-3">Hello, world!</h1>
         <p className="lead">My name is Connor. I'm a senior computer science major at Middlebury College.</p>
         <hr className="my-2" />
-        <p>During my four years at Midd I helped captain the Men's Ultimate Frisbee Team, participated in Middlebury's computer science club, MiddleEndian, and tutored students taking Data Structures and Computer Architecutre.</p>
-        <p>I'm passionate about building sustainable, highly functional products that help people live better lives. My computer science coursework has been directed along a systems track, and I have experience writing low level C programs and peeling back the layers of abstraction that make computers and user applications work. My interests lie in system security, blockchain technology. I'm a fan of the <a href="https://www.dreamsongs.com/RiseOfWorseIsBetter.html" target="_"> Worse Is Better</a> UNIX philosophy and a lover of clean, minimal interfaces. </p>
+        <p>During my four years at Middlebury I helped captain the Men's Ultimate Frisbee Team, participated in our computer science club, MiddleEndian, and tutored students taking Data Structures and Computer Architecutre.</p>
+        <p>I'm passionate about building sustainable, highly functional products that help people live better lives. My computer science coursework has been directed along a systems track. I have experience writing low level C programs and peeling back the layers of abstraction that make computers and applications work. My interests lie in security, systems, and blockchain technology, although I'm always ready to learn new things. I'm a fan of the <a href="https://www.dreamsongs.com/RiseOfWorseIsBetter.html" target="_"> Worse Is Better</a> UNIX philosophy and a lover minimalism. </p>
         <p className="lead">
           <Button href="https://github.com/cchanify" color="primary" target="_blank">Github</Button> {' '}
           <Button href="https://www.linkedin.com/in/connor-hanify-848063114/" color="primary" target="_blank">LinkedIn</Button>
         </p>
       </Jumbotron>
-      <img height="640px" src={img} alt="crater lake" />
+      {/* <img height="650px" src={img} alt="crater lake" /> */}
     </div>
 
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col, Container } from 'reactstrap';
 
-
 class Projects extends Component {
   render() {
     return (
       <Container>
-        <Col sm="12" md={{ size: 8, offset: 2 }} >
-          <Row>
+        <CardText>   </CardText>
+        <Row>
+          <Col>
             <Card body outline color="secondary">
               <CardTitle>Endeavor</CardTitle>
               <CardText>A web application to track job application progress. Created with Chris Fetterolf as a semester long project for our capstone senior work in the computer science major.</CardText>
-              <Button color="primary" target="_blank" href="https://endeavorapp.herokuapp.com/" >Endeavor</Button>
+              <Button color="primary" target="_blank" href="https://endeavorapp.herokuapp.com/">Endeavor</Button>
             </Card>
-          </Row>
-
-          <Row>
+          </Col>
+          
+          <Col>
             <Card body outline color="secondary">
               <CardTitle>NASA Picture of the Day</CardTitle>
               <CardText>A small web application that utilizes a publicly available NASA API to display a different picture and description of our universe each day. Adapted for React from an online guide.</CardText>
-              <Button color="primary" >Go somewhere</Button>
+              <Button href="https://dashboard.heroku.com/apps/nasa-picoftheday" target="_blank" color="primary">The Universe</Button>
             </Card>
-          </Row>
-
-          <Row>
+          </Col>
+        </Row>
+        <CardText>   </CardText>
+        <Row>
+          <Col>
             <Card body outline color="secondary">
               <CardTitle>Todo List</CardTitle>
               <CardText>A todo list I made to learn the basics of React and Javascript.</CardText>
@@ -31,53 +33,58 @@ class Projects extends Component {
               Todo App
               </Button>
             </Card>
-          </Row>
+          </Col>
 
-          <Row>
+          <Col>
             <Card body outline color="secondary">
               <CardTitle>Tic Tac Toe</CardTitle>
               <CardText>A small application made from the React documentation. </CardText>
-              <Button color="primary" target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/" >Tic Tac Toe</Button>
+              <Button color="primary" target="_blank" href="https://blooming-depths-87074.herokuapp.com/" >Tic Tac Toe</Button>
             </Card>
-          </Row>
-        </Col>
+          </Col>
+        </Row>
+
       </Container>
 
 
-
-      /* <Row>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>Endeavor</CardTitle>
-            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-            <Button target="_blank" href="https://endeavorapp.herokuapp.com/" >Endeavor</Button>
-          </Card>
-        </Col>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>Todo List</CardTitle>
-            <CardText>A todo list application I made to learn the basic sof React and Javascript</CardText>
-            <Button target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/">
-            TODO
-            </Button>
-          </Card>
-        </Col>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>Tic Tac Toe</CardTitle>
-            <CardText>A small app from the React documentation deployed on Heroku</CardText>
-            <Button target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/" >Tic Tac Toe</Button>
-          </Card>
-        </Col>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>NASA Picture of the Day</CardTitle>
-            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-            <Button>Go somewhere</Button>
-          </Card>
-        </Col>
-      </Row> */
-
+      // <Container>
+      //   <Col sm="12" md={{ size: 8, offset: 2 }} >
+      //       <CardText>   </CardText>
+      //     <Row>
+      //       <Card body outline color="secondary">
+      //         <CardTitle>Endeavor</CardTitle>
+      //         <CardText>A web application to track job application progress. Created with Chris Fetterolf as a semester long project for our capstone senior work in the computer science major.</CardText>
+      //         <Button color="primary" target="_blank" href="https://endeavorapp.herokuapp.com/">Endeavor</Button>
+      //       </Card>
+      //     </Row>
+      //       <CardText>   </CardText>
+      //     <Row>
+      //       <Card body outline color="secondary">
+      //         <CardTitle>NASA Picture of the Day</CardTitle>
+      //         <CardText>A small web application that utilizes a publicly available NASA API to display a different picture and description of our universe each day. Adapted for React from an online guide.</CardText>
+      //         <Button href="https://dashboard.heroku.com/apps/nasa-picoftheday" target="_blank" color="primary">The Universe</Button>
+      //       </Card>
+      //     </Row>
+      //       <CardText>   </CardText>
+      //     <Row>
+      //       <Card body outline color="secondary">
+      //         <CardTitle>Todo List</CardTitle>
+      //         <CardText>A todo list I made to learn the basics of React and Javascript.</CardText>
+      //         <Button color="primary" target="_blank" href="https://shielded-escarpment-48287.herokuapp.com/">
+      //         Todo App
+      //         </Button>
+      //       </Card>
+      //     </Row>
+      //       <CardText>   </CardText>
+      //     <Row>
+      //       <Card body outline color="secondary">
+      //         <CardTitle>Tic Tac Toe</CardTitle>
+      //         <CardText>A small application made from the React documentation. </CardText>
+      //         <Button color="primary" target="_blank" href="https://blooming-depths-87074.herokuapp.com/" >Tic Tac Toe</Button>
+      //       </Card>
+      //     </Row>
+      //   </Col>
+      // </Container>
     )
   }
 }
