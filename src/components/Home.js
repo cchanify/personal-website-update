e09b897df829cec754af3ee497786b3508617566
Connor <chanify@middlebury.edu>
2018-10-13 19:27:14 -0400

    fixed typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Jumbotron, Button, Container, Row, Col, CardText} from 'reactstrap';
 import styled from 'styled-components';
 
 import img from './images/crater.png';
-import me from './images/me4.jpeg';
+import me from './images/me.jpeg';
 import styles from './main.css';
 
 var imgStyle = {
@@ -20,14 +20,13 @@ const Home = (props) => {
           {/* <p> <img height="200px" src={me} alt="me" /> </p> */}
           <hr className="my-2" />
           <p>During my four years at Middlebury I helped captain the <a href="http://sites.middlebury.edu/ultimate/2018/06/18/middlebury-guy-pranksters-2018-season-recap/" target="_blank">Men's Ultimate Frisbee Team</a>, participated in our computer science club, MiddleEndian, and tutored students taking Data Structures and Computer Architecutre.</p>
-          <p>I'm passionate about building sustainable, highly functional products that help people live better lives. My computer science coursework has been directed along a systems track. I have experience writing low level C programs and peeling back the layers of abstraction that make computers and applications work. My interests lie in security, systems, and blockchain technology, although I'm always ready to learn new things. I'm a fan of the <a href="https://www.dreamsongs.com/RiseOfWorseIsBetter.html" target="_"> Worse Is Better</a> UNIX philosophy and a lover minimalism. </p>
+          <p>I'm passionate about building sustainable, highly functional products that help people live better lives. My computer science coursework has been directed along a systems track. I have experience writing low level C programs and peeling back the layers of abstraction that make computers and applications work. My interests lie in security, systems, and blockchain technology, although I'm always ready to learn new things. I'm a fan of the <a href="https://www.dreamsongs.com/RiseOfWorseIsBetter.html" target="_"> Worse Is Better</a> UNIX philosophy and a lover of minimalism. </p>
           <p className="lead">
             <Button href="https://github.com/cchanify" color="primary" target="_blank">Github</Button> {' '}
             <Button href="https://www.linkedin.com/in/connor-hanify-848063114/" color="primary" target="_blank">LinkedIn</Button>
           </p>
         </Jumbotron>
       </Row></Col></Container>
-{/* <img height="650px" src={img} alt="crater lake" /> */}
     </div>
 
   );
